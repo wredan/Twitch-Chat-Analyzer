157d797421e72dcaf8fae644ec4722f307b0e021
Danilo <danilo.santitto@gmail.com>
2020-06-17 12:06:10 +0200

    Created JSON from message

--- Kafka-Connector/src/main/java/chatmessage/ChatMessageListener.java
+++ Kafka-Connector/src/main/java/chatmessage/ChatMessageListener.java
@@ -2,13 +2,42 @@ package chatmessage;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 
+import com.google.gson.Gson;
+
 public class ChatMessageListener extends ListenerAdapter  {
 	
 	@Override
 	public void onGenericMessage(GenericMessageEvent event) throws Exception {
-		// TODO Auto-generated method stub
 		super.onGenericMessage(event);
 		
-		MessageQueue.getInstance().add(event.getUser().getNick() + ": " + event.getMessage());
+		MessageQueue.getInstance().add(parseToJSON(event));
 	}
+	
+	private String parseToJSON(GenericMessageEvent event) {
+		 Gson gson = new Gson();
+		 String json = gson.toJson(new UserMessage(
+				 event.getUser().getIdent().toString(),
+				 event.getUser().getNick().toString(),
+				 event.getUser().getUserId().toString(),
+				 event.getMessage().toString(),
+				 event.getTimestamp()));
+		  
+		 return json;
+	}
+}
+
+class UserMessage{
+	public String ident;
+	public String nick;
+	public String userId;
+	public String message;
+	public Long timestamp;
+	
+	public UserMessage(String ident, String nick, String userId, String message, Long timestamp) {
+		this.ident = ident;
+		this.nick = nick;
+		this.userId = userId;
+		this.message = message;
+		this.timestamp = timestamp;
+	}	
 }
