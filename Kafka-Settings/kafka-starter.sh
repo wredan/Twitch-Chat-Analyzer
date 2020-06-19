e7412a2f7f13fedb6f827ed896aeaeda04e0f1ec
Danilo <danilo.santitto@gmail.com>
2020-06-19 03:33:58 +0200

    Updated doc

--- Kafka-Settings/kafka-starter.sh
+++ Kafka-Settings/kafka-starter.sh
@@ -1,4 +1,4 @@
 bin/kafka-server-start.sh config/server.properties --override zookeeper.connect=$ZOOKEEPER_SERVER &
-sleep 5s
+sleep 2s
 bin/connect-standalone.sh conf/worker.properties conf/chat-channel.properties
 
