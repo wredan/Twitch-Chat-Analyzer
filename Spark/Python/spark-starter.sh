0f3bf04e547f5f7d5aa24bb3915a3dc09f830eff
Danilo <danilo.santitto@gmail.com>
2020-06-24 11:18:15 +0200

    Added python spark project

---
+++ Spark/Python/spark-starter.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ ./spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.6 /opt/spark/code/spark-consumer.py
---
+++ Spark/Python/sparkConsumerConfig.py
@@ -0,0 +1,26 @@
+
+# set streamer nationality
+twitch_streamer_nationality = "it"
+
+# set your custom emote value to use for sentiment analysis. Keep in mind to set a balanced values for each emote.
+twitch_emotes = {
+    '<3': 0.4,
+    '4head': 1,
+    'babyrage': -0.7,
+    'biblethump': -0.7,
+    'blessrng': 0.3,
+    'bloodtrail': 0.7,
+    'coolstorybob': -1,
+    'residentsleeper': -1,
+    'kappa': 0.3,
+    'lul': -0.3,
+    'pogchamp': 1.5,
+    'heyguys': 1,
+    'wutface': -1.5,
+    'kreygasm': 1,
+    'seemsgood': 0.7,
+    'kappapride': 0.7,
+    'feelsgoodman': 1,
+    'notlikethis': -1
+}
+
