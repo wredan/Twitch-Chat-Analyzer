3a44803cda5664e4f7bece335285b1dea6263812
Danilo <danilo.santitto@gmail.com>
2020-06-29 20:04:59 +0200

    Fix kafka starter

--- Kafka/Kafka-Settings/kafka-starter.sh
+++ Kafka/Kafka-Settings/kafka-starter.sh
@@ -1,3 +1,4 @@
+sleep 7s
 bin/kafka-server-start.sh config/server.properties --override zookeeper.connect=$ZOOKEEPER_SERVER &
 sleep 2s
 bin/connect-standalone.sh conf/worker.properties conf/chat-channel.properties
