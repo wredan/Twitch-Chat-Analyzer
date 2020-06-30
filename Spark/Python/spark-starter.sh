c09391961acb6fa454d750b6c24acd43683ddb25
Danilo <danilo.santitto@gmail.com>
2020-06-30 22:40:56 +0200

    Added elastic jar

--- Spark/Python/spark-starter.sh
+++ Spark/Python/spark-starter.sh
@@ -1,2 +1,3 @@
 #/bin/bash
+sleep 15s
 ./spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.6 --jars /opt/spark/elasticsearch-hadoop-7.8.0.jar --py-files /opt/tap/code/modules.zip /opt/tap/code/sparkConsumer.py
